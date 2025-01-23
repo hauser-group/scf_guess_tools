28fda849c125b9d3b8177d998672128aae07fbc3
Dominik Milacher <dominik.milacher@student.tugraz.at>
2025-01-23 21:14:25 +0100

    Implement wavefunction guessing for psi4

--- scf_guess_tools/psi4/wavefunction.py
+++ scf_guess_tools/psi4/wavefunction.py
@@ -2,26 +2,27 @@ import os
 import psi4
 import re
 
+from ..wavefunction import Wavefunction as Base
 from .auxilary import clean_context
-from .molecule import singlet, non_singlet
-from psi4.core import Molecule, Wavefunction
-from typing import Tuple
+from .molecule import Molecule
+from psi4.core import Matrix, Wavefunction as Native
+from typing import Tuple, Self
 
 
 def _hartree_fock(molecule: Molecule,
                   guess: str,
                   basis: str,
-                  second_order: bool = False)\
-        -> Wavefunction:
+                  second_order: bool = False) \
+        -> Native:
     stability_analysis = "NONE"
-    if non_singlet(molecule):
+    if not molecule.singlet:
         stability_analysis = "FOLLOW"
-    elif molecule.natom() <= 30:
+    elif molecule.atoms <= 30:
         stability_analysis = "CHECK"
 
     psi4.set_options({
         "BASIS": basis,
-        "REFERENCE": "RHF" if singlet(molecule) else "UHF",
+        "REFERENCE": "RHF" if molecule.singlet else "UHF",
         "GUESS": guess,
         # Disable density fitting for highest possible accuracy and
         # because stability analysis is not available for density fitted
@@ -37,7 +38,7 @@ def _hartree_fock(molecule: Molecule,
             "SOSCF_MAX_ITER": 40,
         })
 
-    _, wfn = psi4.energy("HF", molecule=molecule, return_wfn=True)
+    _, wfn = psi4.energy("HF", molecule=molecule.native, return_wfn=True)
     return wfn
 
 
@@ -84,16 +85,16 @@ def _hartree_fock_iterations(stdout_file: str) -> int:
 
 def calculate(molecule: Molecule,
               theory: str,
-              guess: str | Wavefunction,
+              guess: str | Native,
               basis: str)\
-        -> Tuple[Wavefunction, int, bool]:
+        -> Tuple[Native, int, bool]:
     assert theory == "HF"
 
-    if isinstance(guess, Wavefunction):
+    if isinstance(guess, Native):
         # See https://forum.psicode.org/t/custom-guess-for-hartree-fock/2026/6
         scratch_dir = psi4.core.IOManager.shared_object().get_default_path()
         guess.to_file(filename=f"{scratch_dir}/"
-                               f"stdout.{molecule.name()}.{os.getpid()}.180.npy")
+                               f"stdout.{molecule.name}.{os.getpid()}.180.npy")
 
     try:
         with clean_context() as stdout_file:
@@ -115,22 +116,40 @@ def calculate(molecule: Molecule,
     return wfn, iterations, second_order
 
 
-def guess(molecule: Molecule, guess: str, basis: str) -> Wavefunction:
-    with clean_context():
-        psi4.set_options({
-            "BASIS": basis,
-            "GUESS": guess
-        })
+class Wavefunction(Base):
+    def __init__(self, native: Native, molecule: Molecule, origin: str):
+        super().__init__(molecule, origin)
+        self._native = native
 
-        basis = psi4.core.BasisSet.build(molecule, target=basis)
-        ref_wfn = psi4.core.Wavefunction.build(molecule, basis)
-        start_wfn = psi4.driver.scf_wavefunction_factory(
-            name="HF",
-            ref_wfn=ref_wfn,
-            reference="RHF" if singlet(molecule) else "UHF",
-        )
-        start_wfn.form_H()
-        start_wfn.form_Shalf()
-        start_wfn.guess()
+    @classmethod
+    def guess(cls, molecule: Molecule, basis: str, method: str) -> Self:
+        with clean_context():
+            psi4.set_options({
+                "BASIS": basis,
+                "GUESS": method
+            })
 
-        return start_wfn
+            basis = psi4.core.BasisSet.build(molecule.native, target=basis)
+            ref_wfn = psi4.core.Wavefunction.build(molecule.native, basis)
+            start_wfn = psi4.driver.scf_wavefunction_factory(
+                name="HF",
+                ref_wfn=ref_wfn,
+                reference="RHF" if molecule.singlet else "UHF",
+            )
+            start_wfn.form_H()
+            start_wfn.form_Shalf()
+            start_wfn.guess()
+
+            return Wavefunction(start_wfn, molecule, method)
+
+    @property
+    def native(self) -> Native:
+        return self._native
+
+    @property
+    def Da(self) -> Matrix:
+        return self._native.Da_subset("AO")
+
+    @property
+    def Db(self) -> Matrix:
+        return self._native.Db_subset("AO")
