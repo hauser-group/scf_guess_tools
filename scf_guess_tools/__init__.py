163f9b1b96df349007d3e4e7c49bc879e957f1aa
Dominik Milacher <dominik.milacher@student.tugraz.at>
2025-02-16 20:49:56 +0100

    Add more classes to top-level module

--- scf_guess_tools/__init__.py
+++ scf_guess_tools/__init__.py
@@ -1,3 +1,5 @@
 from .psi4.engine import Engine as Psi4Engine
 from .pyscf.engine import Engine as PySCFEngine
 from .metric import Metric
+from .molecule import Molecule
+from .wavefunction import Wavefunction
