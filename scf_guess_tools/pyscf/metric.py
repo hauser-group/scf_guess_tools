90650f524d259a570a073240966a7bb7cc49ca68
Elias Wachmann <elias.wachmann@gmail.com>
2025-02-19 16:37:19 +0100

    f_score pyscf

--- scf_guess_tools/pyscf/metric.py
+++ scf_guess_tools/pyscf/metric.py
@@ -1,8 +1,21 @@
+import numpy as np
+
 from .wavefunction import Wavefunction
 
 
 def f_score(initial: Wavefunction, final: Wavefunction) -> float:
-    return 0
+    Da_guess, Db_guess = initial.Da, initial.Db
+    Da_ref, Db_ref = final.Da, final.Db
+
+    S = initial.molecule.native.intor("int1e_ovlp")
+
+    Q = lambda P_guess, P_ref: np.trace(P_guess @ S @ P_ref @ S)
+    N = lambda P_ref: np.trace(P_ref @ S)
+
+    numerator = Q(Da_guess, Da_ref) + Q(Db_guess, Db_ref)
+    denominator = N(Da_ref) + N(Db_ref)
+
+    return numerator / denominator
 
 
 def diis_error(initial: Wavefunction, final: Wavefunction) -> float:
