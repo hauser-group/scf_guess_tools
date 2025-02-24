50b6b846a7fc8410b4e964d7cbf662b56f948432
Elias Wachmann <elias.wachmann@gmail.com>
2025-02-24 14:02:46 +0100

    diss error

--- scf_guess_tools/pyscf/metric.py
+++ scf_guess_tools/pyscf/metric.py
@@ -19,8 +19,18 @@ def f_score(initial: Wavefunction, final: Wavefunction) -> float:
 
 
 def diis_error(initial: Wavefunction, final: Wavefunction) -> float:
-    return 1
+    Da_guess, Db_guess = initial.Da, initial.Db
+    Da_ref, Db_ref = final.Da, final.Db
+    S = initial.molecule.native.intor("int1e_ovlp")
+
+    Err_a = Da_guess @ Da_ref @ S - S @ Da_guess @ Da_ref
+    Err_b = Db_guess @ Db_ref @ S - S @ Db_guess @ Db_ref
+    Err_a_t = np.trace(Err_a @ Err_a)
+    Err_b_t = np.trace(Err_b @ Err_b)
+    return Err_a_t + Err_b_t
 
 
 def energy_error(initial: Wavefunction, final: Wavefunction) -> float:
+    Da_guess, Db_guess = initial.Da, initial.Db
+    Da_ref, Db_ref = final.Da, final.Db
     return 2
