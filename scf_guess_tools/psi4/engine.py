e329059b9e1f43f3810c31b53625944a5933256d
Dominik Milacher <dominik.milacher@student.tugraz.at>
2025-03-07 16:28:16 +0100

    Move psi4 output file to scratch directory

--- scf_guess_tools/psi4/engine.py
+++ scf_guess_tools/psi4/engine.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 from ..engine import Engine as Base
 from ..molecule import Molecule
 from ..wavefunction import Wavefunction
-from tempfile import TemporaryDirectory
 
+import os
 import psi4
 
 
@@ -12,7 +12,13 @@ class Engine(Base):
     def __init__(self, cache: bool = True, verbose: int = 0, **kwargs):
         super().__init__(f"{self}" if cache else None, verbose)
 
-        self._output_directory = TemporaryDirectory()
+        directory = os.environ.get("PSI_SCRATCH")
+
+        if directory is None:
+            raise RuntimeError("PSI_SCRATCH environment variable not set")
+
+        self._output_directory = f"{directory}/{self}"
+        os.makedirs(self._output_directory, exist_ok=True)
 
         psi4.set_output_file(
             self.output_file, append=False, execute=True, print_header=False
@@ -20,7 +26,7 @@ class Engine(Base):
 
     @property
     def output_file(self) -> str:
-        return f"{self._output_directory.name}/stdout"  # don't rename, bug with READ option
+        return f"{self._output_directory}/stdout"  # don't rename, bug with READ option
 
     def load(self, path: str) -> Molecule:
         from .molecule import Molecule
