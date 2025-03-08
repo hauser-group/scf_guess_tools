248838abb514008e2e8d4a626af1d826a2c38efb
Dominik Milacher <dominik.milacher@student.tugraz.at>
2025-03-08 14:19:11 +0100

    Update imports

--- scf_guess_tools/__init__.py
+++ scf_guess_tools/__init__.py
@@ -1,6 +1,6 @@
 from .engine import Engine
 from .matrix import Matrix
-from .metric import f_score, diis_error, energy_error
+from .metric import FScore, DIISError, EnergyError
 from .molecule import Molecule
 from .wavefunction import Wavefunction
 from .psi4.engine import Engine as PsiEngine
--- tests/common.py
+++ tests/common.py
@@ -6,9 +6,7 @@ def replace_random_digit(original: str, modified: str):
     with open(original, "r", encoding="utf-8") as file:
         header = file.readline()
         header += file.readline()
-        print(header)
         content = file.read()
-        print(content)
 
     positions = [match.start() for match in re.finditer(r"\d", content)]
     assert positions, "file should contain digits"
@@ -20,7 +18,5 @@ def replace_random_digit(original: str, modified: str):
     content = content[:index] + new + content[index + 1 :]
 
     with open(modified, "w", encoding="utf-8") as file:
-        print(header)
-        print(content)
         file.write(header)
         file.write(content)
