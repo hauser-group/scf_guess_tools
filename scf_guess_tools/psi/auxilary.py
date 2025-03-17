9e83b4673adc7fb7c760e9a157e8dc3e8a5f4791
Dominik Milacher <dominik.milacher@student.tugraz.at>
2025-03-17 12:50:42 +0100

    Make psi4 clean_context safer

--- scf_guess_tools/psi/auxilary.py
+++ scf_guess_tools/psi/auxilary.py
@@ -5,11 +5,19 @@ import psi4
 
 @contextmanager
 def clean_context():
+    exception = None
+
     with psi4.driver.p4util.hold_options_state():
         psi4.core.clean_options()
         psi4.core.clean()
 
-        yield
+        try:
+            yield
+        except Exception as e:
+            exception = e
+        finally:
+            psi4.core.clean_options()
+            psi4.core.clean()
 
-        psi4.core.clean_options()
-        psi4.core.clean()
+    if exception is not None:
+        raise exception
